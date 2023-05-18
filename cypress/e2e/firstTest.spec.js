e71c6c4f1b00218cc207db77acd522469a08c08c
Erdoğan Paçacı <EPACACI@ku.edu.tr>
2023-05-18 13:46:37 +0300

    tag ve favorite count ile ilgili çalısma yapıldı

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,8 +1,15 @@
 /// <reference types="cypress" />
 
+const exp = require("constants")
+
 describe("Test with backend", ()=>{
    
     beforeEach('login to the app', ()=>{
+       
+       /**
+        * mack dosyasındaki tags degerlerini kullanmasını sagladık
+        */
+        cy.intercept('GET', 'https://api.realworld.io/api/tags', {fixture : 'tags.json'}) //sayfa ilk acıldıgında kontrol edeceğimiz için once yazıyoruz
         cy.loginToApplication()
     })
 
@@ -17,7 +24,7 @@ describe("Test with backend", ()=>{
      * eger kayıt islemi yapacaksak POST keywordunu kullanıyoruz.
      * as methodu ile bir postArticles instance yaratıp response dan donen degerleri ona atıyoruz
      */
-    it.only("verify correct request and response", ()=>{
+    it("verify correct request and response", ()=>{
 
         cy.intercept('POST', 'https://api.realworld.io/api/articles/').as('postArticles') 
 
@@ -33,6 +40,44 @@ describe("Test with backend", ()=>{
             expect(xhr.request.body.article.body).to.equal('this is body of the article1')
             expect(xhr.response.body.article.description).to.equal('this is a description1')
         })
+    })
+
+
+    /**mock dosyasonda kullandıgımız degerlerin kontrolunu sagladık */
+    it.only('verify popular tags are displayed', ()=>{
+
+        //cy.log('we log in')
+
+        cy.get('.tag-list')
+        .should('contain', 'cypress')
+        .and('contain', 'automation')
+        .and('contain', 'testing')
+
+    })
+
+
+    it.only('verify global feed likes count', ()=>{
+
+        cy.intercept('GET', 'https://api.realworld.io/api/articles/feed*', {"articles":[],"articlesCount":0})
+        cy.intercept('GET', 'https://api.realworld.io/api/articles**', {fixture: "articles.json"})
+
+
+        cy.contains('Global Feed').click()
+        cy.get('app-article-list button').then(heartlist =>{
+
+            expect(heartlist[0]).to.contain('1')
+            expect(heartlist[1]).to.contain('5')
+        })
+
+        cy.fixture('articles').then(file =>{
+            const articleLink=file.articles[1].slug
+            file.articles[1].favoritesCount =6
+            cy.intercept('POST', 'https://api.realworld.io/api/articles/'+articleLink+'/favorite',file)
+
+
+        })
+
+        cy.get('app-article-list button').eq(1).click().should('contain','6')
     })
 
 
